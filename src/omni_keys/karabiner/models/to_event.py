1a080cda4af604498e1adf95aeb81a65b7ca2a9c
ShengJie <shengjie@bytedance.com>
2025-12-20 10:35:51 +0800

    add fields in ToEvent

--- src/omni_keys/karabiner/models/to_event.py
+++ src/omni_keys/karabiner/models/to_event.py
@@ -9,8 +9,19 @@ from .modifier import Modifier
 
 
 class ToEvent(BaseModel):
-    """Karabiner `to` event model."""
+    """
+    Karabiner `to` event model.
+
+    https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/to/
+    """
 
     key_code: Optional[KeyCode] = None
+    shell_command: Optional[str] = None
+    set_variable: Optional[Variable] = None
     modifiers: Optional[List[Modifier]] = None
+
+    
+class Variable(BaseModel):
+    name: str
+    value: str | int
 
